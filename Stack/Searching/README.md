8dd1cf1295dbce6768acc750da5cd16a78f0f759
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-04-07 12:38:35 +0530

    directory changed

---
+++ Stack/Searching/README.md
@@ -0,0 +1,7 @@
+## Linear search
+
+**Linear search is a method for finding a particular value in a list. It works by checking each element in the list one by one until the desired value is found. If the value is not found, the search will return -1.**
+
+## Binary search
+
+**Binary search is a more efficient method for finding a particular value in a list. It works by repeatedly dividing the list in half until the desired value is found. Binary search is only faster than linear search if the list is sorted.**
---
+++ leetcode/141-linked-list-cycle.ts
@@ -0,0 +1,56 @@
+// Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+// Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+// Definition for singly-linked list.
+class ListNode {
+    val: number;
+    next: ListNode | null;
+
+    constructor(val: number) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+type TypeListNode = { val: number, next: ListNode | null };
+
+
+
+
+
+
+
+
+
+// Using hashing
+// Time complexity - O(N)
+// Space complexity - O(N)
+
+var hasCycle = function (head: TypeListNode) {
+
+};
+
+// Making modification in node structure
+// Time complexity - O(N)
+// Space complexity - O(1)
+
+var hasCycle = function (head: TypeListNode) {
+
+};
+
+// Using slow pointer and fast pointer
+// Time complexity - O(N)
+// Space complexity - O(1)
+
+var hasCycle = function (head: TypeListNode) {
+
+};
